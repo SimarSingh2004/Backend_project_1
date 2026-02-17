61a838cb60978338f1bc91c91af661bf5f545f47
Simar <ssimarsingh024@gmail.com>
2026-02-17 20:42:37 +0530

    DB connected

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}`).then(() => {
+      console.log("Connected to MongoDb");
+    });
+    app.on("error", (error) => {
+      console.log("Error ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("Error ", error);
+    throw error;
+  }
+})();
